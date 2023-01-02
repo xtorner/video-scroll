b19bae3f5853fa0398067f6e30b7b96607b72e20
xtorner <xtorner@aikreate.com>
2023-01-02 13:49:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 import video from "./vid.mp4";
+import { NavLink } from "react-bootstrap";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [progress, setProgress] = useState(0);
   const videoEl = useRef(null);
-  // const cursorEl = useRef(null);
+
   const handleLoadedData = () => {
     setIsLoaded(true);
-    alert(isLoaded);
+
     setProgress((videoEl.current.duration / 100) * progress);
   };
+
   const handleTimeUpdate = () => {
     setProgress((videoEl.current.currentTime / videoEl.current.duration) * 100);
   };
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollPos = window.scrollY;
@@ -28,34 +31,52 @@ function App() {
     };
   }, []);
 
-  // useEffect(() => {
-  //   const handleMouseMove = (event) => {
-  //     cursorEl.current.style.top = `${event.clientY}px`;
-  //     cursorEl.current.style.left = `${event.clientX}px`;
-  //     cursorEl.current.style.opacity = `${event.clientY / 1000}`;
-  //   };
-  //   document.addEventListener("mousemove", handleMouseMove);
-  //   return () => {
-  //     document.removeEventListener("mousemove", handleMouseMove);
-  //   };
-  // }, []);
-
   return (
     <>
       <div className="area-top">
         <progress value={progress} max="100" className="progress-bar" />
+
+        <nav className="container navbar navbar-expand-lg">
+          <NavLink className="navbar-brand" href="#">
+            <img src="/img/logo.png" width="50" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <NavLink className="nav-link" href="#"></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#"></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" href="#"></NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
       <div className="video-screen">
         <video
           preload="auto"
           ref={videoEl}
           src={video}
-          onLoadedData={() => handleLoadedData}
+          onLoadedData={handleLoadedData}
           onTimeUpdate={handleTimeUpdate}
         />
       </div>
       <section className="section">Section Uno</section>
-      <section className="section">Section Dos</section>{" "}
+      <section className="section">Section Dos</section>
     </>
   );
 }
