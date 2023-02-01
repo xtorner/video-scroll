1b8fa058715b758ff80c57451e136779af118e0f
xtorner <xtorner@aikreate.com>
2023-02-01 16:41:25 +0100

    Perspective Box Dev

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ImageCarousel from "./components/utils/ImageCarousel";
 import ScrollIntoView from "react-scroll-into-view";
 import Typewriter from "./components/utils/Typewriter";
 import TextHoverEffect from "./components/utils/TextHoverEffect";
+import PerspectiveBox from "./components/utils/PerspectiveBox";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -212,7 +213,7 @@ function App() {
             }
           >
             <video
-              playsInLine
+              playsInline
               muted
               preload="auto"
               ref={videoEl}
@@ -232,39 +233,41 @@ function App() {
             <div id="anchor1" className="anchor1"></div>
           </ScrollIntoView>
           {/* <div ref={anchor1Ref} id="anchor1" className="anchor1"></div> */}
-          <section
-            className={`section section-platform fade ${
-              elements.element2 ? "visible" : "hidden"
-            }`}
-          >
-            <BackgroundWithText>
-              <h2>The Platform</h2>
-              <div>
-                <h3>What we offer</h3>
-                <ul>
-                  <li>Mastery of AI technology</li>
-                  <li>
-                    Learn the ethical implications of AI and the importance of
-                    responsible use of technology
-                  </li>
-                  <li>
-                    Educational resources tailored for students between the ages
-                    of 12 and 17 years old
-                  </li>
-                </ul>
-                <br />
-                <h3>What we do</h3>
-                <ul>
-                  <li>Fun and effective learning along with Kod and Aiki</li>
-                  <li>
-                    Project-based learning by solving real cases relevant to
-                    students' lives
-                  </li>
-                  <li>Learning by doing and being actively involved</li>
-                </ul>
-              </div>
-            </BackgroundWithText>
-          </section>
+          <PerspectiveBox data="section section-platform">
+            <section
+              className={`section section-platform fade ${
+                elements.element2 ? "visible" : "hidden"
+              }`}
+            >
+              <BackgroundWithText>
+                <h2>The Platform</h2>
+                <div>
+                  <h3>What we offer</h3>
+                  <ul>
+                    <li>Mastery of AI technology</li>
+                    <li>
+                      Learn the ethical implications of AI and the importance of
+                      responsible use of technology
+                    </li>
+                    <li>
+                      Educational resources tailored for students between the
+                      ages of 12 and 17 years old
+                    </li>
+                  </ul>
+                  <br />
+                  <h3>What we do</h3>
+                  <ul>
+                    <li>Fun and effective learning along with Kod and Aiki</li>
+                    <li>
+                      Project-based learning by solving real cases relevant to
+                      students' lives
+                    </li>
+                    <li>Learning by doing and being actively involved</li>
+                  </ul>
+                </div>
+              </BackgroundWithText>
+            </section>
+          </PerspectiveBox>
           <div ref={anchor2Ref} id="anchor2" className="anchor2"></div>
           <section
             className={`section section-plans fade ${
@@ -339,6 +342,7 @@ function App() {
               </div>
             </BackgroundWithText>
           </section>
+
           <section
             className={`section section-theteam fade ${
               elements.element5 ? "visible" : "hidden"
@@ -353,7 +357,7 @@ function App() {
                     {
                       src: "img/team/MireiaTorello.jpg",
                       text: "Mireia Torello | Co-founder, CEO",
-                      desc: `<strong>Investment Director and Research Directorwith more than 10 years of experience.</strong> <br /><br />Passionate in sustainability, ethics and the impact of artificial intelligence on youth.<br />Wide international research experience and scientific publications.<br />She holds an EMBA from IESE Business School and a PhD in Earth Sciences from James Cook University, Australia.
+                      desc: `<strong>Investment Director and Research Director with more than 10 years of experience.</strong> <br /><br />Passionate in sustainability, ethics and the impact of artificial intelligence on youth.<br />Wide international research experience and scientific publications.<br />She holds an EMBA from IESE Business School and a PhD in Earth Sciences from James Cook University, Australia.
                       <br /><br /><a href="https://www.linkedin.com/in/mireia-torello-raventos/" class="link-icon-team"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" class="svg-inline--fa fa-linkedin icon-team" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg>&nbsp;@mireia-torello-raventos</a><br />
                       <a href="https://twitter.com/TorelloRaventos" class="link-icon-team"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter" class="svg-inline--fa fa-twitter  icon-team" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"></path></svg>&nbsp;@TorelloRaventos</a>`,
                     },
@@ -369,43 +373,46 @@ function App() {
                     {
                       src: "img/team/JosepCurto.jpg",
                       text: "Josep Curto | Co-founder, Product & Analytics Director",
-                      desc: `Advisor, entrepreneur, data scientist, and professor. Founder, AthenaCore. Member of the advisory board at L’Horitzó School. Academic Director of the Master in Business Intelligence and Big Data Analytics (MIBA) at Universitat Oberta de Catalunya (UOC), associate professor at IE Business School, and casual lecturer at AGSM@UNSW where he gives lectures about responsible AI, data science, data governance, big data and data-driven strategies for future managers and developers. Josep holds a BSc (Hons) in Mathematics (UAB), an MSc (Hons) in Business Intelligence (UOC), and an MSc (Hons) in IT Management (UOC), as well as a top tier MBA (IE Business School). He is the author of numerous academic articles and books related to his professional experience.
+                      desc: `<strong>Advisor, entrepreneur, data scientist, and professor.<br />Founder, AthenaCore.<br />Member of the advisory board at L’Horitzó School.<br /><br />Academic Director of the Master in Business Intelligence and Big Data Analytics (MIBA) at Universitat Oberta de Catalunya (UOC), associate professor at IE Business School</strong>, and casual lecturer at <strong>AGSM@UNSW</strong> where he gives lectures about <strong>responsible AI</strong>, <strong>data science</strong>, data governance</strong>, <strong>big data and data-driven strategies</strong> for future managers and developers. Josep holds a <strong>BSc (Hons) in Mathematics (UAB)</strong>, an <strong>MSc (Hons) in Business Intelligence (UOC)</strong>, and an <strong>MSc (Hons) in IT Management (UOC)</strong>, as well as a <strong>top tier MBA (IE Business School)</strong>. He is the author of numerous academic articles and books related to his professional experience.
                       <br /><br />
-                      www.josepcurto.com
-                      @josepcurto
+                      <a href="www.josepcurto.com">www.josepcurto.com</a><br />
+                      @josepcurto<br />
                       @josepcurto<br /><br />`,
                     },
                     {
                       src: "img/team/FrancescColome.jpg",
                       text: "Francesc Colome | Co-founder, Curriculum Director",
-                      desc: "Member of the advisory board at L’Horitzó School. He has a degree in Chemistry and has been, among other responsibilities, inspector of education, Director General of Vocational Training of the Ministry of Education, Secretary of Educational Policies of the Department of Education of the Generalitat, and Professor of Chemistry at the UAB.",
+                      desc: "<strong>Member of the advisory board at L’Horitzó School</strong>.<br />He has a <strong>degree in Chemistry</strong> and has been, among other responsibilities, <strong>inspector of education</strong>, <strong>Director General of Vocational Training of the Ministry of Education</strong>, <strong>Secretary of Educational Policies of the Department of Education of the Generalitat</strong>, and <strong>Professor of Chemistry at the UAB</strong>.",
                     },
                     {
                       src: "img/team/DavidCabanillas.jpg",
                       text: "David Cabanillas | Co-founder, Content Director",
-                      desc: "Data scientist and professor. Member of AthenaCore, and professor at Universitat Oberta de Catalunya (UOC) where he gives lectures about data science. Data passionate with more than 10 experienced years. Helping several companies to become data-driven organizations focused on identifying and creating efficient ways to collect, cleanse and maintain data, and providing decision intelligence insights (based on classification, scoring and propensity algorithms). David holds Ph.D. in Artificial Intelligence (UPC).",
+                      desc: "<strong>Data scientist and professor. <br />Member of AthenaCore, and professor at Universitat Oberta de Catalunya (UOC)</strong> where he gives lectures about data science. Data passionate with more than 10 experienced years.<br />Helping several companies to become data-driven organizations focused on identifying and creating efficient ways to collect, cleanse and maintain data, and providing decision intelligence insights (based on classification, scoring and propensity algorithms).<br />David holds <strong>Ph.D. in Artificial Intelligence (UPC)</strong>.",
                     },
                     {
                       src: "img/team/XaviTorner.jpg",
                       text: "Xavier Torner | Full Stack Engineer",
-                      desc: `Computer Science Engineer with more than 20 years of experience related to web development and system administration.<br />
-As a Site Reliability Engineer (SRE), is responsible of implementation and supervision of DevOps technologies and a cyber secure development infrastructure.`,
+                      desc: `<strong>Computer Science Engineer</strong> with more than 20 years of experience related to web development and system administration.<br /><br />
+As a <strong>Site Reliability Engineer (SRE)</strong>, is responsible of implementation and supervision of DevOps technologies and a cyber secure development infrastructure.`,
                     },
                   ]}
                 ></ImageCarousel>
               </div>
             </BackgroundWithText>
           </section>
+
           <section
             className={`section section-1-4 fade ${
               elements.element6 ? "visible" : "hidden"
             }`}
           >
-            <BackgroundWithText>
-              <h2>Contact Us</h2>
+            <PerspectiveBox>
+              <BackgroundWithText>
+                <h2>Contact Us</h2>
 
-              <p>CONTACT FORM</p>
-            </BackgroundWithText>
+                <p>CONTACT FORM</p>
+              </BackgroundWithText>
+            </PerspectiveBox>
           </section>
           <section className="section section-1-5">
             <div className={classFooter} ref={footer}>
---
+++ src/components/utils/PerspectiveBox.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const Constrain = 450;
+
+const Example = ({ children }, { props }) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [xy, setXY] = useState([0, 0]);
+  const ex1LayerRef = useRef(null);
+
+  const transforms = (x, y, el) => {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - box.height / 2) / Constrain;
+    let calcY = (x - box.x - box.width / 2) / Constrain;
+
+    return `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (isMouseOver) {
+        setXY([e.clientX, e.clientY]);
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [isMouseOver]);
+
+  useEffect(() => {
+    if (isMouseOver) {
+      ex1LayerRef.current.style.transform = transforms(
+        ...xy,
+        ex1LayerRef.current
+      );
+    }
+  }, [isMouseOver, xy]);
+
+  return (
+    <div
+      id="ex1"
+      className={`${props} container3d`}
+      onMouseEnter={() => setIsMouseOver(true)}
+      onMouseLeave={() => setIsMouseOver(false)}
+    >
+      <div id="ex1-layer" className="box3d" ref={ex1LayerRef}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Example;
