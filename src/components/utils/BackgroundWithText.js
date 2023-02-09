431ac0e2da3bc32a4e47627c78efff9ebcd5e092
xtorner <xtorner@aikreate.com>
2023-02-09 10:35:53 +0100

    errates

--- src/components/utils/BackgroundWithText.js
+++ src/components/utils/BackgroundWithText.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import * as noise from "noisejs";
 
 const BackgroundWithText = ({ children }) => {
-  const [fill, setFill] = useState(getRandomColor);
+  const [fill, setFill] = useState(getGradientColor);
   const [cloudPoints, setCloudPoints] = useState([]);
   useEffect(() => {
-    setFill(getRandomColor());
+    setFill(getGradientColor());
     createCloudShape();
   }, []);
 
@@ -13,8 +13,8 @@ const BackgroundWithText = ({ children }) => {
     const n = new noise.Noise(Math.random());
     let points = "";
     for (let a = 0; a < 8 * Math.PI; a += 0.005) {
-      const x = 300 / 5 + 140 * n.perlin2(a, a) * Math.cos(a * a);
-      const y = 60 / 0.98 + 60 * n.perlin2(a, a) * Math.sin(a * Math.sin(a));
+      const x = 300 / 5 + 180 * n.perlin2(a, a) * Math.cos(a * a);
+      const y = 60 / 0.98 + 130 * n.perlin2(a, a) * Math.sin(a * Math.sin(a));
       points += `${x.toFixed(2)},${y.toFixed(2)} `;
     }
     // console.log(points);
@@ -25,31 +25,20 @@ const BackgroundWithText = ({ children }) => {
     <>
       <div className="child">{children}</div>
       <svg width="100%" height="100%" className="eseuve">
-        {<span>`M ${cloudPoints} Z`</span>}
+        <defs>
+          <linearGradient id="black-to-white" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stop-color="#feeded" />
+            <stop offset="100%" stop-color="#ffffff" />
+          </linearGradient>
+        </defs>
         {cloudPoints && <path fill={fill} d={`M ${cloudPoints} Z`} />}
       </svg>
     </>
   );
 };
 
-const getRandomColor = () => {
-  const color1 = "#ffffff";
-  const color2 = "#aaaaaa";
-
-  function blendColors(color1, color2, ratio = 0.5) {
-    const color1Array = color1.match(/\w{2}/g).map((x) => parseInt(x, 16));
-    const color2Array = color2.match(/\w{2}/g).map((x) => parseInt(x, 16));
-    const blended = color1Array.map((c, i) =>
-      Math.round(c + (color2Array[i] - c) * ratio)
-    );
-    return `#${blended.map((x) => x.toString(16).padStart(2, "0")).join("")}`;
-  }
-
-  const colors = Array.from({ length: 10 }, (_, i) =>
-    blendColors(color1, color2, i / 10)
-  );
-  let newFill = colors[Math.floor(Math.random() * colors.length)];
-  return newFill;
+const getGradientColor = () => {
+  return `url(#black-to-white)`;
 };
 
 export default BackgroundWithText;
