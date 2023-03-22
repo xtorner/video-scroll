3ef6d83e41d8201c111edc7f5d037b48bbd966ed
xtorner <xtorner@aikreate.com>
2023-03-22 08:26:17 +0100

    Video 2 New Positions

--- src/App.js
+++ src/App.js
@@ -162,7 +162,7 @@ function App() {
         element2: sY > 176 && sY < 380 ? !elements.element2 : elements.element2,
         element3: sY > 500 && sY < 760 ? !elements.element3 : elements.element3,
         element4:
-          sY > 760 && sY < 1130 ? !elements.element4 : elements.element4,
+          sY > 780 && sY < 1130 ? !elements.element4 : elements.element4,
         element5:
           sY > 1150 && sY < 1400 ? !elements.element5 : elements.element5,
         element6: sY > 1410 ? !elements.element6 : elements.element6,
